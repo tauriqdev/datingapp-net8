95ffd72e62178d89bbd34e66c585e16d05b9c14e
Tauriq Hendricks <tauriqh786@gmail.com>
2024-07-23 12:24:33 +0200

    Enf of Section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var entity = await context.Users.FindAsync(id);
+
+        if (entity is null)
+            return NotFound();
+
+        return Ok(entity);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var list = await context.Users.ToListAsync();
+        return Ok(list);
+    }
+}
