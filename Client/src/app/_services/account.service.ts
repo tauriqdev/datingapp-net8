0b2df64d87d08e86217beb5109ee8e14fe1031df
Tauriq Hendricks <tauriqdev@gmail.com>
2024-07-24 12:00:01 +0200

    End of Section 5

---
+++ Client/src/app/_services/account.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  private http = inject(HttpClient);
+  baseUrl = 'https://localhost:5001/api/'
+
+  currentUser = signal<User | null>(null);
+
+  login(model: any): Observable<void> {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map(user => {
+        if (user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
+  }
+
+  register(model: any): Observable<User> {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if (user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+
+        return user;
+      })
+    );
+  }
+  
+}
